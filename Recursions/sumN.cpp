0151e590fbf566168c5e53ec9d602e53999a468d
Lennox Mountain <lennoxmountain67@gmail.com>
2024-03-28 21:43:08 +0200

    recursions added

---
+++ Recursions/sumN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n){
+    //base conditon (condition to terminate rescursive computing)
+    if (n == 0) return 0;
+
+    
+    
+}
+
+int main(){
+    return 0;
+}

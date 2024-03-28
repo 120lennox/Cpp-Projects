c1a300a5015a5ab2f439fa8b4d85c3eddc435518
Lennox Mountain <lennoxmountain67@gmail.com>
2024-03-28 22:02:19 +0200

    first problem

--- Recursions/sumN.cpp
+++ Recursions/sumN.cpp
@@ -4,11 +4,18 @@ using namespace std;
 int sum(int n){
     //base conditon (condition to terminate rescursive computing)
     if (n == 0) return 0;
+    
+    //Recursive function
+    int result = n + sum(n - 1);
 
-    
-    
+    return result;   
 }
 
 int main(){
+    int n = 0;
+    cout<<"Enter number: ";
+    cin>>n;
+    int result = sum(n);
+    cout<<"sum = "<<result<<endl;
     return 0;
 }

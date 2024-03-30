dbf01b220dc74c3bf85b3fdf38f867d82f12d084
Lennox Mountain <lennoxmountain67@gmail.com>
2024-03-30 22:13:09 +0200

    implementation of undo, redo using stack data structure

---
+++ Practicing Stack/redo_undo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+template <typename input> // this allows you to write code with any datatype
+
+void undo(input data){
+    stack<input> newStack;
+    newStack.pop(input);
+}
+
+template <typename T>
+
+void red(T data){
+    stack<T> newStack;
+    newStack.push(T)
+}

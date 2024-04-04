61495190d4786a0c95f6881e801c9c191d380616
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-04 09:08:41 +0200

    cpp lab lab work

---
+++ class work/memory.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    long num;
+    short number;
+    double var;
+    std::string message;
+
+    std::cout<<"size of long = "<<sizeof(num)<<std::endl;
+    std::cout<<"size of short = "<<sizeof(number)<<std::endl;
+    std::cout<<"size of double = "<<sizeof(var)<<std::endl;
+    std::cout<<"size of string = "<<sizeof(message)<<std::endl;
+}

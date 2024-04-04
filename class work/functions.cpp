4b41fa8ee9274d59b36edd71ca8e0a00fba42a68
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-04 10:18:49 +0200

    function declarations and namespaces

--- class work/functions.cpp
+++ class work/functions.cpp
@@ -3,6 +3,8 @@
  * This program displays correct function definitions
  * the incorrect ones are commented out
 */
+#include<iostream>
+#include <string>
 
 int add(int num1, int num2);
 int add(int num1, float num2);
@@ -11,7 +13,24 @@ int add(int num1, float num2);
 //void add(int num1, int num2);
 
 /**
- * @author Lennox 
+ * @author Lennox Mountain
  * The code below explains the namespaces
 */
 
+namespace lennox {
+    int number; 
+    void printName();
+}
+
+//using namespaces 
+int main(){
+    lennox::number = 120;
+    std::cout<<lennox::number<<std::endl;
+}
+
+/**
+ * namespace directives 
+ * "using directives": saves you from writing the namespace name everytime you wan to use a variable
+*/
+
+using namespace lennox;

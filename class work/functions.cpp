bbad95d2f5be3fef16698659e998ea69fd45a813
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-04 10:10:52 +0200

    correct function declarations

---
+++ class work/functions.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author Lennox Mountain
+ * This program displays correct function definitions
+ * the incorrect ones are commented out
+*/
+
+int add(int num1, int num2);
+int add(int num1, float num2);
+
+/*illegal function declaration. oonly differs the return type but the paarameters are the same*/
+//void add(int num1, int num2);
+
+/**
+ * @author Lennox 
+ * The code below explains the namespaces
+*/
+

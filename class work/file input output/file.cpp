408f9bb9699507bf5a190c73b02cab7866995255
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-15 22:05:32 +0200

    file management added in class work

---
+++ class work/file input output/file.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//reading from a file 
+void read_word(){
+    ifstream read("output.txt");
+    
+    string word1;
+    char word2;
+    string word3;
+    char word4;
+    string word5;
+
+    read>>word1>>word2>>word3>>word4>>word5;
+    cout<<word1<<word2<<word3<<word4<<word5<<endl;
+
+}
+
+//writing in a file
+void write_word(){
+    ofstream write;
+    write.open("output.txt");
+
+    write<<"word"<<" "<<"is"<<" "<<"written"<<endl;
+    write.close();
+}
+
+int main(){
+
+}

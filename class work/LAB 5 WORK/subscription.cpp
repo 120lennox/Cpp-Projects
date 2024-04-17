3c58be9412f32d436ee0592ab34a3e6020b4e0e2
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-17 23:02:17 +0200

    added ctime anf cstdlib

--- class work/LAB 5 WORK/subscription.cpp
+++ class work/LAB 5 WORK/subscription.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+
 void evaluateSubscripption(){
+    //seed the rand() function so you get a different number everytime it's run
+    srand(time(0));
+    int daysUntilExpiration = rand();
+
+    if (daysUntilExpiration == 0){
+        cout<<"You have an active subscription"<<endl;
+        
+    }
+
     
 }

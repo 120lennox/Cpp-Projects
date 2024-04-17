c0be007b27c2c87238b8e571bac07238d1b10910
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-17 23:16:18 +0200

    tried to implement algorithm of qeustion 1

--- class work/LAB 5 WORK/subscription.cpp
+++ class work/LAB 5 WORK/subscription.cpp
@@ -10,8 +10,23 @@ void evaluateSubscripption(){
     int daysUntilExpiration = rand();
 
     if (daysUntilExpiration == 0){
+        cout<<"You have an active subscription"<<endl; 
+    }
+
+    else if(daysUntilExpiration <= 1){
         cout<<"You have an active subscription"<<endl;
-        
+    }
+
+    else if(daysUntilExpiration <= 5){
+        cout<<"You have an active subscription"<<endl;
+    }
+
+    else if(daysUntilExpiration <= 10){
+        cout<<"You have an active subscription"<<endl;
+    }
+
+    else{
+        cout<<"You have an active subscription"<<endl;
     }
 
     

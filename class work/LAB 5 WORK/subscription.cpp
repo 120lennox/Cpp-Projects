aa1d5cdd7287e03ed15eb4f0b362f878cd31e296
Lennox Mountain <lennoxmountain67@gmail.com>
2024-04-17 23:22:52 +0200

    code completed but not run

--- class work/LAB 5 WORK/subscription.cpp
+++ class work/LAB 5 WORK/subscription.cpp
@@ -10,24 +10,24 @@ void evaluateSubscripption(){
     int daysUntilExpiration = rand();
 
     if (daysUntilExpiration == 0){
-        cout<<"You have an active subscription"<<endl; 
+        cout<<"Your subscription has expired."<<endl; 
     }
 
     else if(daysUntilExpiration <= 1){
-        cout<<"You have an active subscription"<<endl;
+        cout<<"Your subscription expires within a day!"<<endl;
+        cout<<"Renew now and save 20%!"<<endl;
     }
 
     else if(daysUntilExpiration <= 5){
-        cout<<"You have an active subscription"<<endl;
+        cout<<"Your subscription expires in "<<daysUntilExpiration<<endl;
+        cout<<"Renew now and save 10%!"<<endl;
     }
 
     else if(daysUntilExpiration <= 10){
-        cout<<"You have an active subscription"<<endl;
+        cout<<"Your subscription will expire soon. Renew now!"<<endl;
     }
 
     else{
         cout<<"You have an active subscription"<<endl;
-    }
-
-    
+    }    
 }
